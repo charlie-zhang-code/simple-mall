dc6da82e9018b74f956a50a46bb76a3b961b4cc6
charlie-zhang-code <charlie_zhang_mail@163.com>
2025-02-05 19:00:16 +0800

    upload

--- src/main/java/io/github/charlie/MainApplication.java
+++ src/main/java/io/github/charlie/MainApplication.java
@@ -1,9 +1,11 @@
 package io.github.charlie;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
  * @Author: charlie-zhang-code
@@ -12,8 +14,21 @@ import org.springframework.context.ConfigurableApplicationContext;
  */
 @SpringBootApplication
 @MapperScan("io.github.charlie.mapper")
+@Slf4j
 public class MainApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(MainApplication.class, args);
+        ConfigurableEnvironment environment = run.getEnvironment();
+        log.info("""
+                                                \s
+                         -------------------------------------------------
+                             Application is running! Access URLs:
+                             Local:    http://localhost:{}
+                             Doc:      http://localhost:{}/doc.html
+                         -------------------------------------------------
+                        \s""",
+                environment.getProperty("server.port"),
+                environment.getProperty("server.port")
+        );
     }
 }
