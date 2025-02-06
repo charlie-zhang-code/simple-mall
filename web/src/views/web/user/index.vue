1744fbdea95a66dad3f8d2b9e12297c2f4f3d9b1
charlie-zhang-code <charlie_zhang_mail@163.com>
2025-02-06 17:28:33 +0800

    upload

--- web/src/views/web/user/index.vue
+++ web/src/views/web/user/index.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
-
+// $route.params.id
 </script>
 
 <template>
-  <div class="flex flex-col">
-    用户详情页  {{ $route.params.id }}
+  <div class="flex flex-col gap-4">
+    <el-image class="w-full h-200px" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+      fit="cover" />
+    <div class="flex flex-col items-center gap-2">
+      <el-avatar :size="50" />
+      <el-text size="large">用户名</el-text>
+      <el-text>网龄 5 个月</el-text>
+    </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
